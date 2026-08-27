f276d2f624a2bbb79d2ae185bf1ecfa102f3fa5b
Evren Baber <elbaber@stu.naperville203.org>
2026-08-27 13:48:15 -0500

    Did warmup 1

---
+++ Warmup.java
@@ -0,0 +1,15 @@
+
+/**
+ * Write a description of class Warmup here.
+ *
+ * @author Evren
+ * @version August 27 2026
+ */
+public class Warmup
+{
+    public static void warmup1() {
+        World myWorld = new World();
+        Turtle pumpkin = new Turtle(myWorld);
+        pumpkin.forward(25);
+    }
+}
