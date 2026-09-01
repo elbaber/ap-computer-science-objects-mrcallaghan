992ef8e83f51cf918b1872685a6d366087918d76
Evren Baber <elbaber@stu.naperville203.org>
2026-09-01 14:06:58 -0500

    Finished notes on strings

---
+++ String1.java
@@ -0,0 +1,20 @@
+
+/**
+ * Write a description of class String1 here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class String1
+{
+    public static void main() {
+        // Replace "smallTestString" with the name you used for your lowercase string
+        String smallTestString = new String( "whisper" );       // Proper string definition
+        String bigTestString = smallTestString.toUpperCase(); 
+        System.out.println(bigTestString);
+        System.out.println(smallTestString);
+        String Hello = "Hello, World";                          // String literal(More common)
+        int n = 7;
+        System.out.println(Hello+n);
+    }
+}
