1cc6782666d7d8e1e04554a45c41faf5c2a34ac6
Evren Baber <elbaber@stu.naperville203.org>
2026-09-02 14:24:46 -0500

    Started TurtleLab

--- TurtleLab.java
+++ TurtleLab.java
@@ -6,10 +6,28 @@ import java.awt.Color;
  * @author (your name)
  * @version (a version number or a date)
  */
-public class TurtleLab
-{
-    public static void main(String[] args)
-    {
+public class TurtleLab {
+    World  Pacific = new World();
+    
+    Turtle Turtle1 = new Turtle(Pacific);
+    Turtle Turtle2 = new Turtle(Pacific);
+    
+    public static void main(String[] args) {
+        
+    }
+    
+    public static void drawStarOfGivenRadius(Turtle turtle, int radius) {
+        int pentagramExteriorAngle = 324;
+        int pentagramInteriorAngle = 360-pentagramExteriorAngle;
+        double cosineOfInteriorAngle = Math.cos(pentagramInteriorAngle);
+        int sideLength             = radius * cosineOfInteriorAngle.intValue();
+        
+        turtle.penUp();
+        turtle.setHeading(0);
+        turtle.forward(radius);
+        turtle.turn(pentagramExteriorAngle/2);
+        turtle.penDown();
+        
         
     }
 }
