f9f0098f062794c8b419184ef3a802c035c4e2ca
Evren Baber <elbaber@stu.naperville203.org>
2026-09-04 13:24:39 -0500

    Worked on TurtleLab and made it prettier

--- TurtleLab.java
+++ TurtleLab.java
@@ -7,7 +7,7 @@ import java.awt.Color;
  * @version (a version number or a date)
  */
 public class TurtleLab {
-    static World Pacific  = new World();
+    static World  Pacific = new World();
     
     static Turtle turtle1 = new Turtle(Pacific);
     static Turtle turtle2 = new Turtle(Pacific);
@@ -16,13 +16,17 @@ public class TurtleLab {
         drawStarOfGivenRadius(turtle1, 100);
     }
     
+    public static void drawsmth(){ //figure out what to draw
+        
+    }
+    
     public static void drawStarOfGivenRadius(Turtle turtle, int radius) {
-        int pentagramExteriorAngle   = 324;
-        int pentagramInteriorAngle   = 360-pentagramExteriorAngle;
-        double cosineOfInteriorAngle = Math.cos(pentagramInteriorAngle/2);
-        double sideLength            = radius * cosineOfInteriorAngle * 2;
-        int intSideLength            = (int) sideLength;
-        int turnAngle                = 180 - pentagramInteriorAngle;
+        int    pentagramExteriorAngle = 324;
+        int    pentagramInteriorAngle = 360-pentagramExteriorAngle;
+        double cosineOfInteriorAngle  = Math.cos(pentagramInteriorAngle/2);
+        double sideLength             = radius * cosineOfInteriorAngle * 2;
+        int    intSideLength          = (int) sideLength;
+        int    turnAngle              = 180 - pentagramInteriorAngle;
 
         turtle.penUp();
         turtle.setHeading(0);
@@ -30,10 +34,9 @@ public class TurtleLab {
         turtle.turn(pentagramExteriorAngle/2);
         turtle.penDown();
         
-        for(int i = 0; i < 5; i++) {
+        for (int i = 0; i < 5; i++) {
             turtle.forward(intSideLength);
             turtle.turn(turnAngle);
         }
-        
     }
 }
